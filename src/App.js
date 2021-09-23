580ebc348326dcaef2b54a7edd68ba0b98739fce
Zack Lyon <zack.j.lyon@gmail.com>
2021-09-22 18:01:06 -0700

    Add router feature

--- src/App.js
+++ src/App.js
@@ -1,62 +1,31 @@
-import './App.css';
-import React, { Component } from 'react';
-import Header from './Header.js';
-import ImageList from './ImageList.js';
-import images from './data.js';
-import Dropdown from './Dropdown/Dropdown.js';
-import Acknowledgement from './Acknowledgement.js';
+import React, { Component } from 'react'
+import {
+    BrowserRouter as Router, 
+    Route, 
+    Switch,
+} from 'react-router-dom';
+import Main from './Main.js'
+import Splash from './Splash.js'
 
 export default class App extends Component {
-  state = {
-    keyword: '',
-    horns: ''
-  }
-
-  handleKeywordChange = (e) => {
-    this.setState({keyword: e.target.value});
-  }
-
-  handleHornsChange = (e) => {
-    this.setState({horns: e.target.value});
-  }
-
-  render() {
-    //filter images for keywords and horns then filter which images are included in both
-    const filteredKeywordImages = images.filter(image => (!this.state.keyword) || image.keyword === this.state.keyword);
-
-    const filteredHornsImages = images.filter(el => (!this.state.horns) || el.horns === Number(this.state.horns));
-
-    const filteredImages = filteredKeywordImages.filter(item => filteredHornsImages.includes(item) === true);
-
-    // filter keyword and horns options available
-    const allKeywords = images.map(image => image.keyword);
-    const filteredKeywordOptions = [...new Set(allKeywords)]; //remove duplicates
-    filteredKeywordOptions.sort();
-
-    const allHorns = images.map(image => image.horns);
-    const filteredHornsOptions = [...new Set(allHorns)]; //remove duplicates
-    filteredHornsOptions.sort((a, b) => a - b);
-
-
-    return (
-      <div className="App">
-        <Header />
-        <Dropdown 
-          filteredOptions={filteredKeywordOptions}
-          handleChange={this.handleKeywordChange}
-        />
-        <Dropdown 
-          filteredOptions={filteredHornsOptions}
-          handleChange={this.handleHornsChange}
-        />
-        <ImageList 
-        keyword={this.state.keyword}
-        selectedImages={filteredImages}
-        />
-        <Acknowledgement />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Router>
+                    <Switch>
+                        <Route 
+                            path="/" 
+                            exact
+                            render={(routerProps) => <Splash {...routerProps} />} 
+                        />
+                        <Route 
+                            path="/main" 
+                            exact
+                            render={(routerProps) => <Main {...routerProps} />} 
+                        />
+                    </Switch>
+                </Router>
+            </div>
+        )
+    }
 }
-
-// now I know that main branches aren't made by default!
